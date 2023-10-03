59c3b6c4cc0792107d03d004f6645222c2f7aa6b
yusun97 <yusun.apply@outlook.com>
2023-10-03 08:29:03 -0700

    Ac fix

--- src/Engine/SpreadSheetClient.ts
+++ src/Engine/SpreadSheetClient.ts
@@ -282,8 +282,25 @@ class SpreadSheetClient {
             });
     }
 
+    /**
+     * AC fix
+     * clear all formulas
+     */
     public clearFormula(): void {
-        return;
+        const requestClearFormulaURL = `${this._baseURL}/document/clear/formula/${this._documentName}`;
+
+        fetch(requestClearFormulaURL, {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "userName": this._userName })
+        })
+            .then(response => {
+                return response.json() as Promise<DocumentTransport>; 
+            }).then((document: DocumentTransport) => {
+                this._updateDocument(document);
+         });
     }
 
 
--- src/Engine/SpreadSheetController.ts
+++ src/Engine/SpreadSheetController.ts
@@ -312,20 +312,21 @@ export class SpreadSheetController {
       return;
     }
     const userEditing = this._contributingUsers.get(user);
-    if (!userEditing) {
-      return;
-    }
-    if (userEditing.cellLabel === '') {
+    if (!userEditing!.isEditing) {
       return;
     }
 
-    userEditing.formulaBuilder.setFormula([]);
+    if (userEditing!.cellLabel === '') {
+      return;
+    }
+
+    userEditing!.formulaBuilder.setFormula([]);
     let cellBeingEdited = this._contributingUsers.get(user)?.cellLabel;
 
     // this should not empty but just in case throw error
     if (cellBeingEdited) {
       let cell = this._memory.getCellByLabel(cellBeingEdited);
-      cell.setFormula(userEditing.formulaBuilder.getFormula());
+      cell.setFormula(userEditing!.formulaBuilder.getFormula());
       this._memory.setCellByLabel(cellBeingEdited, cell);
     }
     this._calculationManager.evaluateSheet(this._memory);
